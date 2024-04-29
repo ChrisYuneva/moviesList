ef74fd24e4350c9d4d43ab196ba21b34388c5f24
ChrisYuneva <christina_yuneva@mail.ru>
2024-04-29 18:34:11 +0400

    fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ command }) => {
   return {
-    base: mode === 'development' ? '' : '/moviesList/',
+    base: command === 'serve' ? '' : '/moviesList/',
     plugins: [react()],
   };
 });
